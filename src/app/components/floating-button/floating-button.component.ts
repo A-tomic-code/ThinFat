d8bf344530ab1fc09a8ca750b12db002c858e923
a-tomic-code <danielvazquezheredia1994@gmail.com>
2022-11-19 19:49:07 +0100

    front clientes listo

--- src/app/components/floating-button/floating-button.component.ts
+++ src/app/components/floating-button/floating-button.component.ts
@@ -13,45 +13,27 @@ export class FloatingButtonComponent implements OnInit {
   socket: WebSocket | null = null;
   conversation : ChatMessage[] = [];
 
+  message: string = '';
   socketConfig: any = {};
 
 
-  constructor(private chatService: ChatService, private renderer: Renderer2) { }
+  constructor(private chatService: ChatService) { }
 
   toggleDisplay(){
     this.expanded ? this.expanded = false : this.expanded = true;
   }
 
-  initSocket(socket:WebSocket){
-    socket.onopen = () => {
-      // alert('Conectado websockets')
-    }
-
-    socket.onmessage = (event) => {
-      const message: ChatMessage = {from: 'xx', to: 'xx', content: event.data};
-      
-      if(event.data[0] === '{'){
-        this.socketConfig(event.data);
-      }else{
-        this.conversation.push(message);
-      }
-      
-    }
-  }
-  
-  setSocketConfig(data: string) {
-    console.log(`Configuring server --> ${data}`)
-
-    const config = JSON.parse(data);
-    this.socketConfig = {...this.setSocketConfig, config}
+  sendMessage(msg:string){
+    this.chatService.sendMessage(msg)
   }
 
+  onSubmit(){
+    this.sendMessage(this.message);
+    this.message = '';
+  }
   ngOnInit(): void {
-    this.chatService.getSocket()
-    ? this.socket = this.chatService.getSocket()
-    : this.socket = this.chatService.connect(false);
-
-    this.socket ? this.initSocket(this.socket) : null;
+    this.socket = this.chatService.getSocket();
+    this.conversation = this.chatService.getConversation()
   }
 
 }
--- src/app/shared/chat.service.ts
+++ src/app/shared/chat.service.ts
@@ -7,29 +7,87 @@ import { ChatMessage } from '../models/chat-message';
 })
 export class ChatService {
 
-  url:string = 'ws://localhost:8085'
+  url:string = 'ws://localhost:8095'
   socket:WebSocket | null = null;
-
+  conversation : ChatMessage[] = [];
+  config : any = {};
 
   constructor(private http: HttpClient) {
-    this.connect(false)
+  }
+
+  sendMessage(msg:string){
+    console.log(`Enviando --> ${msg}`)
+    const message = {
+      type: 'message',
+      content: msg
+    }
+
+    const messageObj = JSON.stringify(message)
+    this.socket?.send(messageObj);
   }
 
   getSocket(){
-    return this.socket;
+    return this.socket ? this.socket : this.connect(false);
+  }
+  
+  getConversation(){
+    return this.conversation;
   }
   
   closeConecction(){
     this.socket ? this.socket.close() : null;
   }
   
-  connect(admin:boolean): WebSocket{
-    this.socket = new WebSocket(this.url);
+  private connect(admin:boolean): WebSocket{
+    const socket = new WebSocket(this.url);
+    this.socket = socket;
+    this.initSocket(this.socket);
   
     if(this.socket.readyState === 1){
       //dispatch action
     }
 
-    return this.socket;
+    return socket;
+  }
+
+  private initSocket(socket:WebSocket){
+    socket.onopen = () => {
+      console.log('Conectado websockets');
+    }
+
+    socket.onmessage = (event) => {
+      const msg = JSON.parse(event.data);
+      const message: ChatMessage = {
+        from: msg.from || 'xx',
+        to: msg.to || 'xx',
+        content: msg.content
+      };
+      
+      switch(msg.type){
+        case 'message':
+          this.conversation.push(message);
+          console.log('Recibido --> ', message);
+          break;
+        case 'config':
+
+          for (const key in msg){
+            
+            if(key != 'type'){
+              this.config[key] = msg[key];
+            }
+
+          }
+
+          console.log('New config --> ', this.config);
+          break;   
+      };
+
+      // if(event.data[0] === '{'){
+      //   this.socketConfig(event.data);
+      // }else{
+      //   this.conversation.push(message);
+      // }
+      
+    }
   }
 }
